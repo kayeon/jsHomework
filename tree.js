4cb8a2b80ea575c3d3205ba2779c49b4ca01f5b8
Josh Stewart <josh@extropicstudios.com>
2016-06-03 03:43:11 +0000

    mark root node

--- tree.js
+++ tree.js
@@ -9,7 +9,7 @@ var node2 = new TreeNode(null, null, 2);
 var node3 = new TreeNode(null, null, 3);
 var node4 = new TreeNode(node1, null, 4);
 var node5 = new TreeNode(node2, node3, 5);
-var node6 = new TreeNode(node4, node5, 6);
+var node6 = new TreeNode(node4, node5, 6); // root node
 
 function isLeaf(node) {
   // TODO
